d420725d5e6135a77fcd6af79e570361b76aeef0
unknown <ritivakil3@gmail.com>
2023-11-21 09:30:28 +0530

    Update docker

--- server/src/axios/axios.client.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"; //allows us to communicate with APIs easily in our React apps. 
-const get =async(url) =>{
-    const response = await axios.get(url);
-    return response.data;
-};
-
-export default {get};
-
